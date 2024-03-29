7222ed63bdffe8b19438e2ec6dc70453d0e31d4c
Juan Chan <josedelinux@hotmail.com>
2024-03-29 21:37:13 +0800

    bug fix

--- fish/.config/fish/conf.d/abbr.fish
+++ fish/.config/fish/conf.d/abbr.fish
@@ -1,5 +1,5 @@
 if status is-interactive
   abbr --add cl clear
-  abbr --add vifzf vim (fzf)
+  abbr --add vifzf 'vim (fzf)'
   abbr --add csgen 'clang-format -style=google -dump-config > .clang-format'
 end
