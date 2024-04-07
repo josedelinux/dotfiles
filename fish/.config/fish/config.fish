ae06a7bbc099d0caa437184156795b611dd4700c
Juan Chan <josedelinux@hotmail.com>
2024-04-07 10:13:25 +0800

    new file:   fish/.config/fish/conf.d/path.fish 	modified:   fish/.config/fish/config.fish 	modified:   git/.gitconfig

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,7 @@ source ~/.config/fish/conf.d/variables.fish
 source ~/.config/fish/conf.d/exports.fish
 source ~/.config/fish/conf.d/abbr.fish
 source ~/.config/fish/conf.d/alias.fish
+source ~/.config/fish/conf.d/path.fish
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
