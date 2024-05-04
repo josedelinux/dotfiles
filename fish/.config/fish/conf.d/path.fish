ce01b5e8700d03f2df35057fa013dbafe2566c00
Juan Chan <josedelinux@hotmail.com>
2024-05-04 11:04:07 +0800

    fix path

--- fish/.config/fish/conf.d/path.fish
+++ fish/.config/fish/conf.d/path.fish
@@ -1,2 +1,8 @@
 fish_add_path /home/$USER/.cargo/bin/
 fish_add_path /home/$USER/go/bin/
+
+# for wsl
+set virt_env (systemd-detect-virt)
+if string match -q "wsl" $virt_env
+  fish_add_path /mnt/c/Windows/ # explorer.exe
+end
