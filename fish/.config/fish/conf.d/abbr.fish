b845a353ee2a3a0cf94b5791690eb5a8160b9e74
Juan Chan <josedelinux@hotmail.com>
2024-05-06 08:27:17 +0800

    add abbr

--- fish/.config/fish/conf.d/abbr.fish
+++ fish/.config/fish/conf.d/abbr.fish
@@ -4,4 +4,7 @@ if status is-interactive
   abbr --add vani 'vim -u /dev/null'
   abbr --add csgen 'clang-format -style=google -dump-config > .clang-format'
   abbr --add gitfu 'git fetch; git status'
+
+  # i type fast
+  abbr --add tmx 'tmux'
 end
