d4aa214387c6736827c9d64f100e0bbba106fe71
Juan Chan <josedelinux@hotmail.com>
2024-10-06 11:30:02 +0800

    add nvfzf

--- fish/.config/fish/conf.d/abbr.fish
+++ fish/.config/fish/conf.d/abbr.fish
@@ -2,6 +2,7 @@ if status is-interactive
   abbr --add unset 'set --erase'
   abbr --add cl clear
   abbr --add vifzf 'vim (fzf)'
+  abbr --add nvfzf 'nvim (fzf)'
   abbr --add vani 'vim -u /dev/null'
   abbr --add vini 'vim -u /dev/null'
   abbr --add vihis 'nvim ~/.local/share/fish/fish_history'
