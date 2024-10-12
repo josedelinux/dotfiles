8cb92a03abe0ab0d35b0e196b7d77c73b77c26ba
Juan Chan <josedelinux@hotmail.com>
2024-10-12 19:50:57 +0800

    add ohmyposh for fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,17 +3,27 @@
 # *.fish scripts in ~/.config/fish/conf.d/ are also automatically executed before config.fish.
 
 set USE_STARSHIP false  # Change to false to disable starship
+set USE_OMP true  # Change to false to disable oh-my-posh
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
 if command -v zoxide > /dev/null
-  zoxide init fish | source
+    zoxide init fish | source
 end
 
 if test $USE_STARSHIP = true
     if command -v starship > /dev/null
         starship init fish | source
+    end
+end
+
+if test $USE_OMP = true
+    if command -v oh-my-posh > /dev/null
+        oh-my-posh init fish | source
+        # oh-my-posh init fish --config ~/atomic.omp.json | source
+        # oh-my-posh init fish --config "https://github.com/JanDeDobbeleer/oh-my-posh/raw/refs/heads/main/themes/atomic.omp.json" | source
+
     end
 end
