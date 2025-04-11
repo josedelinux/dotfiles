e9f3056ca839395244a81aed1848428d9a3b4935
Juan Chan <josedelinux@hotmail.com>
2025-04-11 10:01:34 +0800

    add fish direnv

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,6 +13,10 @@ if command -v zoxide > /dev/null
     zoxide init fish | source
 end
 
+if command -v direnv > /dev/null
+    direnv hook fish | source
+end
+
 if test $USE_STARSHIP = true
     if command -v starship > /dev/null
         starship init fish | source
