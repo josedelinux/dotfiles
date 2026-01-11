114fd6cdc8c34ed8f20e77d3060d7fd4afb00c9e
Juan Chan <josedelinux@hotmail.com>
2026-01-11 10:10:04 -0500

    fish: add new abbr

--- fish/.config/fish/conf.d/abbr.fish
+++ fish/.config/fish/conf.d/abbr.fish
@@ -1,23 +1,25 @@
 if status is-interactive
-  abbr --add unset 'set --erase'
-  abbr --add cl clear
-  abbr --add vifzf 'vim (fzf)'
-  abbr --add nvfzf 'nvim (fzf)'
-  abbr --add vani 'vim -u /dev/null'
-  abbr --add vini 'vim -u /dev/null'
-  abbr --add vihis 'nvim ~/.local/share/fish/fish_history'
-  abbr --add csgen 'clang-format -style=google -dump-config > .clang-format'
-  abbr --add gitfu 'git fetch; git status'
+    abbr --add unset 'set --erase'
+    abbr --add cl clear
+    abbr --add vifzf 'vim (fzf)'
+    abbr --add nvfzf 'nvim (fzf)'
+    abbr --add vani 'vim -u /dev/null'
+    abbr --add vini 'vim -u /dev/null'
+    abbr --add vihis 'nvim ~/.local/share/fish/fish_history'
+    abbr --add csgen 'clang-format -style=google -dump-config > .clang-format'
+    abbr --add gitfu 'git fetch; git status'
 
-  # i type fast
-  abbr --add tmx 'tmux'
-  abbr --add lg 'lazygit'
+    # i type fast
+    abbr --add tmx tmux
+    abbr --add lg lazygit
 
-	# makefile abbr
-	abbr --add mb 'make -B'
-	abbr --add mc 'make clean'
+    # makefile abbr
+    abbr --add mb 'make -B'
+    abbr --add mc 'make clean'
 
-  # docker
-  abbr --add docker-revive 'docker start -a -i (docker ps -q -l)'
+    abbr --add r 'tcc -run'
+
+    # docker
+    abbr --add docker-revive 'docker start -a -i (docker ps -q -l)'
 
 end
